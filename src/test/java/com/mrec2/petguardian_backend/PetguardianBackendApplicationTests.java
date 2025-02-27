0e0c1c7d36f26fd9f9a56e26a8d56577a9239975
Mrec2 <marioteleco@outlook.com>
2025-02-27 16:28:12 +0100

    Backend empty project created

---
+++ src/test/java/com/mrec2/petguardian_backend/PetguardianBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrec2.petguardian_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetguardianBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
