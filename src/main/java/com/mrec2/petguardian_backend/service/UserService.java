52561dc5380eab2ffd35e99612f52879eadf0ae6
Mrec2 <marioteleco@outlook.com>
2025-03-08 21:13:46 +0100

    BCrypt is used for encrypting the password. SecurityConfig and UserService was modified

--- src/main/java/com/mrec2/petguardian_backend/service/UserService.java
+++ src/main/java/com/mrec2/petguardian_backend/service/UserService.java
@@ -3,6 +3,7 @@ package com.mrec2.petguardian_backend.service;
 import com.mrec2.petguardian_backend.models.User;
 import com.mrec2.petguardian_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +14,12 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     public User createUser(User user) {
+        String encryptedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);
         return userRepository.save(user);
     }
 
