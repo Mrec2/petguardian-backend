18d9dd19e888fa096ac21df6cec06ac1f521862b
Mrec2 <marioteleco@outlook.com>
2025-03-08 21:19:59 +0100

    Password verification was added for login

--- src/main/java/com/mrec2/petguardian_backend/controller/AuthController.java
+++ src/main/java/com/mrec2/petguardian_backend/controller/AuthController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.mrec2.petguardian_backend.service.UserAuthService;
-
 import java.util.Map;
 
 @RestController
--- src/main/java/com/mrec2/petguardian_backend/service/UserAuthService.java
+++ src/main/java/com/mrec2/petguardian_backend/service/UserAuthService.java
@@ -1,11 +1,10 @@
 package com.mrec2.petguardian_backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import com.mrec2.petguardian_backend.models.UserAuth;
 import com.mrec2.petguardian_backend.repository.UserAuthRepository;
-
 import java.util.Optional;
 
 @Service
@@ -14,12 +13,16 @@ public class UserAuthService {
     @Autowired
     private UserAuthRepository userAuthRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder; 
+
     public boolean login(String email, String password) {
         Optional<UserAuth> userOptional = userAuthRepository.findByEmail(email);
         if (userOptional.isPresent()) {
             UserAuth user = userOptional.get();
-            return user.getPassword().equals(password); 
+            
+            return passwordEncoder.matches(password, user.getPassword());
         }
-        return false; 
+        return false;
     }
 }
